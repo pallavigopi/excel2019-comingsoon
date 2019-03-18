55d38a07f8e546630467aee0ff586e9fd09dda4a
nevilleantony98 <nevilleantony98@gmail.com>
2019-03-18 19:20:17 +0530

    edited some css

--- src/components/excel-about/index.js
+++ src/components/excel-about/index.js
@@ -6,7 +6,7 @@ export default class ExcelAbout extends Component {
     
     render() {
         return(
-                <div className={styles["header-about"]}>
+                <div className={styles["excel-details"]}>
                     <p className={styles["about-text"]}>First tech fest in the world, better than the rest.</p>
                     <p className={styles["about-text"]}>Random bullshit about how excel is gonna  make the world a better place.</p>
                     <p className={styles["about-text"]}>20th Edition blah blah, ask content team for the rest.</p>
