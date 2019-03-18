8e3d46d255c61cd24b6dd45543e917d320265f05
nevilleantony98 <nevilleantony98@gmail.com>
2019-03-18 19:01:57 +0530

    divided page into components and bit of formatting

---
+++ src/components/excel-about/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import styles from './style.module.css';
+
+
+export default class ExcelAbout extends Component {
+    
+    render() {
+        return(
+                <div className={styles["header-about"]}>
+                    <p className={styles["about-text"]}>First tech fest in the world, better than the rest.</p>
+                    <p className={styles["about-text"]}>Random bullshit about how excel is gonna  make the world a better place.</p>
+                    <p className={styles["about-text"]}>20th Edition blah blah, ask content team for the rest.</p>
+                </div>
+        );
+    }
+}
