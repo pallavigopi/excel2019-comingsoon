6eabf3a96bb992b33e842b58d96adacee60b6e5e
nevilleantony98 <nevilleantony98@gmail.com>
2019-03-19 19:40:38 +0530

    Desktop responsiveness mostly done

--- src/components/excel-about/index.js
+++ src/components/excel-about/index.js
@@ -7,9 +7,11 @@ export default class ExcelAbout extends Component {
     render() {
         return(
                 <div className={styles["excel-details"]}>
-                    <p className={styles["about-text"]}>First tech fest in the world, better than the rest.</p>
-                    <p className={styles["about-text"]}>Random bullshit about how excel is gonna  make the world a better place.</p>
-                    <p className={styles["about-text"]}>20th Edition blah blah, ask content team for the rest.</p>
+                    <div className={styles["abouttext"]}>
+                        <p className={styles["about-text"]}>First tech fest in the world, better than the rest.</p>
+                        <p className={styles["about-text"]}>Random bullshit about how excel is gonna  make the world a better place.</p>
+                        <p className={styles["about-text"]}>20th Edition blah blah, ask content team for the rest.</p>
+                    </div>
                 </div>
         );
     }
