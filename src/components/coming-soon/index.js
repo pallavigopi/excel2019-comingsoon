2159aed7dfa9fe1e3d13b1a019fda78acc4aafe0
Nikhil713 <nikhilbabu213@gmail.com>
2019-04-03 08:37:29 +0530

    Add countdown timer

--- src/components/coming-soon/index.js
+++ src/components/coming-soon/index.js
@@ -14,19 +14,19 @@ export default class ComingSoon extends Component {
                 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
                 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
                 var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-                document.getElementById("demo").innerHTML = days + " : "  
+                document.getElementById("timer").innerHTML = days + " : "  
                 + hours + " : " + minutes + " : " + seconds; 
                     if (t < 0) { 
                         clearInterval(x); 
-                        document.getElementById("demo").innerHTML = "EXPIRED"; 
+                        document.getElementById("timer").innerHTML = "EXPIRED"; 
                     } 
             }, 1000); 
         return(
             <div className={styles["coming-soon"]}>
                 <div className = {styles["gear-gif"]}></div>
-                <div>
+                <div className = {styles["countdown"]}>
                     <div className = {styles["comingSoon"]}>COMING SOON</div>
-                    <p id="demo"></p>
+                    <div id="timer" className={styles["timer"]}></div>
                 </div>
               
             </div>
